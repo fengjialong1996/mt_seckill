73a6e45c42ea017250da42ed3ad19208412297de
fengjialong1996 <fengjialong1996@163.com>
2023-03-24 20:03:17 +0800

    tianmao_seckill

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.remote import webelement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+
 import datetime
 import time
 import logging
@@ -39,10 +42,10 @@ class ChromeDriver():
 def login_tianmao(driver):
     driver.get(platform2url["tianmao"]["login_url"])
     driver.switch_to.frame(driver.find_element(by=By.ID, value='J_loginIframe'))
-    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'iconfont icon-password')))
     driver.find_element(by=By.ID, value='fm-login-id').send_keys(platform2act_pwd["tianmao"]["account"])
     driver.find_element(by=By.ID, value='fm-login-password').send_keys(platform2act_pwd["tianmao"]["password"])
     driver.find_element(by=By.CLASS_NAME, value='fm-btn').click()
+    # tianmao_unlock(driver)
     while 'login' in driver.current_url:
         print(driver.current_url)
         time.sleep(1)
@@ -55,24 +58,37 @@ def login(driver, platform):
         case _:
             print("input platform error")
 
-def buy_on_time(driver, buytime):
-
-    driver.get('https://www.maotai.com/')
-    driver.find_element_by_link_text('茅台商城').click()
-    time.sleep(3)
-
-    while True:
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-        if now > buytime:
-            driver.find_element_by_id('btn-reservation').click()
-            driver.find_element_by_link_text('立即抢购').click()
-            time.sleep(0.5)
-            driver.find_element_by_link_text('去购物车结算').click()
-            driver.find_element_by_link_text('去结算').click()
-            driver.find_element_by_link_text('提交订单').click()
-            print('抢购成功！')
-            break
-        time.sleep(0.1)
+def tianmao_unlock(driver: webdriver.Chrome):
+    """
+    execute moving block
+    """
+    # try:
+    print("move the block...")
+    # WebDriverWait(driver, 6).until(
+    #     EC.presence_of_element_located((By.ID, "#baxia-dialog-content"))
+    # )
+    time.sleep(6)
+    driver.switch_to.frame(driver.find_element(by=By.ID, value="baxia-dialog-content"))
+    # WebDriverWait(driver,2).until(
+    #     EC.frame_to_be_available_and_switch_to_it((By.ID,"#baxia-dialog-content"))
+    # )
+    bar_element = driver.find_element(by=By.CLASS_NAME, value='nc-lang-cnt')
+    print("11111")
+    slider_width = bar_element.size["width"]
+    slider_height = bar_element.size["height"]
+    slider_x = bar_element.location["x"]
+    slider_y = bar_element.location["y"]
+    print("slider_width:", slider_width)
+    move_distance = slider_width - 100
+    actions = ActionChains(driver)
+    actions.move_to_element(bar_element).perform()
+    actions.drag_and_drop_by_offset(bar_element, move_distance, 0).perform()
+    print("bar_element:", bar_element)
+    time.sleep(0.5)
+    # except Exception as e:
+    #     print(u"move the block failed!!! Error: %s" % e)
+    # if driver.find_element(by=By.CLASS_NAME, value='nc-lang-cnt'):
+    #     tianmao_unlock(driver)
 
 if __name__ == '__main__':
     # 1. init browser driver
@@ -85,28 +101,18 @@ if __name__ == '__main__':
     chrome_driver.get(platform2url["tianmao"]["maotai_url"])
     
     # 4. buy on time
-    # time.sleep(6)
-    # buy_button_element_text = ""
-    # while not buy_button_element_text:
-    #     buy_button_element = chrome_driver.find_element(by=By.CSS_SELECTOR, value="[class*=Actions--leftButtons]")
-    #     buy_button_element_text = buy_button_element.text
-    # buy_button_element = chrome_driver.find_element(by=By.CSS_SELECTOR, value="[class*=Actions--leftButtons]")
-    # buy_button_element_text = buy_button_element.text
-    # print("buy_button_element_text:", type(buy_button_element_text), buy_button_element_text)
-    # WebDriverWait(chrome_driver, 10).until(
-    #     EC.text_to_be_present_in_element((By.CSS_SELECTOR, "[class*=Actions--leftButtons]"), "商品已经卖光啦，非常抱歉")
-    # )
     buy_button_element = chrome_driver.find_element(by=By.CSS_SELECTOR, value="[class*=Actions--leftButtons]")
     buy_button_element_text = buy_button_element.text
     while True:
-        print("buy_button_element_text:", type(buy_button_element_text), buy_button_element_text)
         current_timestamp = int(time.time() * 1000)
-        if current_timestamp > platform2start_timestamp["tianmao"] - 180000:
-        # if True:
+        if platform2start_timestamp["tianmao"] > current_timestamp > platform2start_timestamp["tianmao"] - 300000:
             logger.info("Nowtime: {}, Timestamp:{}. seckill will start in about 3 min".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), int(time.time() * 1000)))
-            # buy_button_element.refresh()
-            WebDriverWait(chrome_driver, 240).until(
-                EC.text_to_be_present_in_element((By.CSS_SELECTOR, "[class*=Actions--leftButtons]"), "立即购买")
+            print("buy_button_element_text:", type(buy_button_element_text), buy_button_element_text)
+            WebDriverWait(chrome_driver, 6).until(
+                EC.text_to_be_present_in_element((By.CSS_SELECTOR, "[class*=Actions--leftButtons]"), "即将开始，03.24 20:00开售")
+            )
+            WebDriverWait(chrome_driver, 360).until_not(
+                EC.text_to_be_present_in_element((By.CSS_SELECTOR, "[class*=Actions--leftButtons]"), "即将开始，03.24 20:00开售")
             )
             buy_button_element = chrome_driver.find_element(by=By.CSS_SELECTOR, value="[class*=Actions--leftButtons]")
             buy_button_element_text = buy_button_element.text
@@ -116,13 +122,13 @@ if __name__ == '__main__':
                 for i in range(10):
                     buy_button_element.click()
                     print("botton clicked")
-                    time.sleep(0.1)
+                    time.sleep(0.2)
                 while True:
                     if 'detail' not in chrome_driver.current_url:
                         continue
             except NoSuchElementException:
                 print("click botton failed")
         else:
-            time.sleep(60)
+            logger.warning("Nowtime: {}, Timestamp:{}. Please check the time!!!".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), int(time.time() * 1000)))
     print("closing browser")
 
